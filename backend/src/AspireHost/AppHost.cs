1ab9a4e9a90baecd1faeb5c15e85c273a7e09939
mminetti <mnlminetti@gmail.com>
2026-08-22 19:34:39 -0300

    worker

--- backend/src/AspireHost/AppHost.cs
+++ backend/src/AspireHost/AppHost.cs
@@ -1,5 +1,4 @@
-﻿using System.Net.Sockets;
-
+﻿
 var builder = DistributedApplication.CreateBuilder(args);
 
 // Add SQL Server container
@@ -7,13 +6,18 @@ var sqlServer = builder.AddSqlServer("sqlserver")
   .WithLifetime(ContainerLifetime.Persistent);
 
 // Add the database
-var cleanArchDb = sqlServer.AddDatabase("db");
+var db = sqlServer.AddDatabase("db");
 
 // Add the web project with the database connection
 builder.AddProject<Projects.Web>("api")
-  .WithReference(cleanArchDb)
-  .WithEnvironment("ASPNETCORE_ENVIRONMENT", builder.Environment.EnvironmentName)
-  .WaitFor(cleanArchDb);
+    .WithReference(db)
+    .WithEnvironment("ASPNETCORE_ENVIRONMENT", builder.Environment.EnvironmentName)
+    .WaitFor(db);
+
+builder.AddProject<Projects.Worker>("worker")
+    .WithReference(db)
+    .WithEnvironment("ASPNETCORE_ENVIRONMENT", builder.Environment.EnvironmentName)
+    .WaitFor(db);
 
 builder
   .Build()
---
+++ backend/src/Worker/Program.cs
@@ -0,0 +1,4 @@
+﻿var builder = Host.CreateApplicationBuilder(args);
+builder.Services.AddHostedService<Worker.Worker>();
+var host = builder.Build();
+host.Run();
