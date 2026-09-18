a2e4476f0042aca0e2b84cbcabd4ddc454e697e8
mminetti <mnlminetti@gmail.com>
2026-09-18 12:58:14 -0300

    missing migration

---
+++ backend/src/Infrastructure/Data/Migrations/20260918155725_ConnectorTypeSeedNameUpdate.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class ConnectorTypeSeedNameUpdate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "ConnectorType",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Name",
+                value: "FTP");
+
+            migrationBuilder.UpdateData(
+                table: "ConnectorType",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Name",
+                value: "File Storage");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "ConnectorType",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Name",
+                value: "ftp");
+
+            migrationBuilder.UpdateData(
+                table: "ConnectorType",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Name",
+                value: "file.storage");
+        }
+    }
+}
